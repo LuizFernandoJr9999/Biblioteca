2026295592f7003cca3d5ce85040de6634400e35
Luiz Fernando <luizfernandojunior9999@gmail.com>
2023-12-26 17:30:35 -0300

    Meio aula 02 - Sistema de Livros - 53'14''

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,3 +1,6 @@
+from usuarios.models import Usuario
 from django.contrib import admin
 
-# Register your models here.
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    readonly_fields = ('nome' , 'email' , 'senha',)
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Usuario
+from django.shortcuts import redirect 
+from hashlib import sha256
 
 def login(request):
     return HttpResponse('login')
 
 def cadastro(request):
-    return HttpResponse('cadastro')
+    return render(request, 'cadastro.html')
+
+def valida_cadastro(request):
+    nome=request.POST.get('nome')
+    senha=request.POST.get('senha')
+    email=request.POST.get('email')
+
+    usuario = Usuario.objects.filter(email = email)
+
+    if len(nome.strip()) == 0 or len(email.strip()) == 0:
+        return redirect('/auth/cadastro/?status=1')
+
+    if len(senha) < 8:
+        return redirect('/auth/cadastro/?status=2')
+
+    if len(usuario) > 0:
+        return redirect('/auth/cadastro/?status=3')
+
+    try:
+        senha = sha256(senha.encode()).hexdigest()
+        usuario = Usuario(nome = nome, 
+                          senha = senha, 
+                          email= email)
+        usuario.save()
+
+        return redirect('/auth/cadastro/?status=0')
+    except:
+        return redirect('/auth/cadastro/?status=4')
+
+    #return HttpResponse(f"{nome} {senha} {email}")
