12ca9fffa44ebdaddc7ab8d2458d434cee5363dd
Luiz Fernando <luizfernandojunior9999@gmail.com>
2023-12-31 00:15:08 -0300

    Durante  aula 03 - Criando categoria, linkando e listando livros - Sistema de Livros - 34'03''

--- livro/views.py
+++ livro/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from usuarios.models import Usuario
+from .models import Livros
 # Create your views here.
 
 def home(request):
     if request.session.get('usuario'):
-        usuario = Usuario.objects.get(id = request.session['usuario']).nome 
+        usuario = Usuario.objects.get(id = request.session['usuario'])
+        livros = Livros.objects.filter(usuario = usuario)
         #return HttpResponse(f'Olá {usuario}')
-        return HttpResponse(f'Olá {usuario}')
+        return render(request, 'home.html', {'livros': livros})
     else:
         return redirect('/auth/login/?status=2')
