dac352d6bcadf19b99a20ad16951fabac51411ba
Luiz Fernando <luizfernandojunior9999@gmail.com>
2024-01-06 20:18:52 -0300

    Durante aula 05 - Cadastro de livros e cálculo de duração - Sistema de Livros - 10'52''

---
+++ livro/templatetags/filtros.py
@@ -0,0 +1,11 @@
+from django import template 
+
+register = template.Library()
+
+@register.filter()
+def mostra_duracao(value1, value2):
+    #return type(value1 - value2)
+    
+    
+    return (value1 - value2).days
+    
